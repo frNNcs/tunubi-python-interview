b247de503b666058dcd1afec819916e14a45bed8
Francisco <frnncs@gmail.com>
2022-07-01 10:06:58 -0300

    add & apply flake8 formating

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, request, json, Response
-from flask import Flask, request, json, Response
+from flask import Flask
+from flask import request
+from flask import json
+from flask import Response
+
 from pymongo import MongoClient
 import datetime
 from bson import ObjectId
 
+
 print("connecting database...")
 client = MongoClient(host='db')
 print("connection established with Mongo")
+
 
 class MongoAPI:
     def __init__(self, document):
@@ -14,12 +19,13 @@ class MongoAPI:
         self.collection = cursor[document]
 
     def read(self):
-        documents =  self.collection.find()
-        output = [{item: str(data[item]) for item in data} for data in documents]
+        documents = self.collection.find()
+        output = [{item: str(data[item]) for item in data}
+                  for data in documents]
         return output
 
-    def find_byid(self,id):
-        return self.collection.find_one({"_id" : ObjectId(id)})
+    def find_byid(self, id):
+        return self.collection.find_one({"_id": ObjectId(id)})
 
     def write(self, data):
         new_document = data
@@ -28,37 +34,42 @@ class MongoAPI:
         return str(result.inserted_id)
 
 
-app = Flask(__name__)   
+app = Flask(__name__)
+
 
 @app.route('/')
 def base():
     return "status:up"
 
+
 @app.route('/addAnswer')
 def answers_post():
     data = request.json
     res = MongoAPI("answers").write(data)
-    return Response(response=json.dumps(res),status=200,mimetype='application/json')
+    return Response(response=json.dumps(res),
+                    status=200,
+                    mimetype='application/json')
+
 
 @app.route('/addPoll')
 def polls_post():
     data = request.json
     res = MongoAPI("polls").write(data)
-    return Response(response=json.dumps(res),status=200,mimetype='application/json')    
+    return Response(response=json.dumps(res),
+                    status=200,
+                    mimetype='application/json')
+
 
 @app.route('/getPolls')
 def polls_get():
     polls = MongoAPI("polls").read()
     answers = MongoAPI("answers").read()
     for poll in polls:
-        poll['answers'] = [answer for answer in answers if answer['poll_id'] == poll['_id']]
+        poll['answers'] = [
+            answer for answer in answers if answer['poll_id'] == poll['_id']]
     return Response(response=json.dumps(polls))
 
 
 if __name__ == "__main__":
     print('up')
     app.run(host='0.0.0.0', debug=True)
-
-
-
-
